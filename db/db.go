6528e3bdf7b68972aeed950f9696b1113db9895d
wangzn <wzpzning@gmail.com>
2015-03-17 16:23:13 +0800

    add query

--- db/db.go
+++ db/db.go
@@ -10,7 +10,13 @@ import (
 )
 
 var (
+  Row []string
+)
+
+var (
   DBList map[string]*DB
+  Conn  *sql.DB
+  Result []Row
 )
 
 type DB struct {
@@ -29,11 +35,45 @@ func Set(addrs, name string) (r bool) {
     log.Fatal(err)
     return false
   }
-  defer db.Close()
   var d DB
   d = DB{db, addrs, ""}
   DBList[name] = &d
   return true
+}
+
+func Active(name string) (r bool) {
+  if val, ok := DBList[name]; ok {
+    Conn = val.Link
+    return true
+  } else {
+    return false
+  }
+}
+
+func Query(sql string)  {
+  rows, _ := Conn.Query("SELECT * FROM pub_ip_map;")
+  columns, _ := rows.Columns()
+  count := len(columns)
+  values := make([]interface{}, count)
+  valuePtrs := make([]interface{}, count)
+
+  for rows.Next() {
+    for i, _ := range columns {
+      valuePtrs[i] = &values[i]
+    }
+    rows.Scan(valuePtrs ...)
+    for i, col := range columns {
+      var v interface{}
+      val := values[i]
+      b, ok := val.([]byte)
+      if (ok) {
+        v = string(b)
+      } else {
+        v = val
+      }
+      fmt.Println(col, v)
+    }
+  }
 }
 
 func ListDB() {
