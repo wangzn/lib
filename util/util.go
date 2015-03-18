f8b748368584dfea239be8839d1221edb835f554
wangzn <wzpzning@gmail.com>
2015-03-18 16:41:29 +0800

    add some funcs to util

--- util/util.go
+++ util/util.go
@@ -4,9 +4,12 @@ import (
     "bufio"
     "io/ioutil"
     "os"
+    "net/http"
+    "strings"
+
     )
 
-func IsExist(path string) bool {
+func FExist(path string) bool {
   _, err := os.Stat(path)
   if err == nil {
     return true
@@ -15,33 +18,101 @@ func IsExist(path string) bool {
   if os.IsNotExist(err) {
     return false
   }
-
   return false
 }
 
-func IsNotExist(path string) bool {
-  return (!IsExist(path))
+func FNExist(path string) bool {
+  return (!FExist(path))
 }
 
-func FileGetContents(fn string) ([]byte, error) {
-  fp, err := os.OpenFile(fn, os.O_RDONLY, os.ModePerm)
-  if err != nil {
-    return nil, err
+func HRun(method, url, reqBody string) ([]byte, error) {
+  var req *http.Request
+  var err error
+  client := &http.Client{}
+  if len(reqBody) > 0 {
+    req, err = http.NewRequest(method, url, strings.NewReader(reqBody))
+    req.ContentLength = int64(len(reqBody))
+  } else {
+    req, err = http.NewRequest(method, url, nil)
   }
-  defer fp.Close()
-  reader := bufio.NewReader(fp)
-  contents, _ := ioutil.ReadAll(reader)
-  return contents, nil
+  resp, err := client.Do(req)
+  if err != nil {
+    return []byte(""), err
+  }
+  defer resp.Body.Close()
+  contents, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return []byte(""), err
+  }
+  return contents, err
 }
 
-func FilePutContents(fn string, con []byte) error {
+func HGet(url string) ([]byte, error) {
+  resp, err := http.Get(url)
+  if err != nil {
+    return []byte(""), err
+  }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return []byte(""), err
+  }
+  return body, err
+}
+
+func HPost(url, reqBody, bodyType string) ([]byte, error) {
+  if bodyType == "" {
+    bodyType = "application/x-www-form-urlencoded"
+  }
+  resp, err := http.Post(url, bodyType, strings.NewReader(reqBody))
+  if err != nil {
+    return []byte(""), err
+  }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return []byte(""), err
+  }
+  return body, nil
+}
+
+func HPut(url, reqBody string) ([]byte, error) {
+  return HRun("PUT", url, reqBody)
+}
+
+func HDelete(url, reqBody string) ([]byte, error) {
+  return HRun("DELETE", url, reqBody)
+}
+
+func FileGetContents(fn string) (contents []byte, err error) {
+  if strings.HasPrefix(fn, "http://") {
+    return HGet(fn)
+  } else {
+    fp, err := os.OpenFile(fn, os.O_RDONLY, os.ModePerm)
+    if err != nil {
+      return []byte(""), err
+    }
+    defer fp.Close()
+    reader := bufio.NewReader(fp)
+    con, _ := ioutil.ReadAll(reader)
+    contents = con
+  }
+  return
+}
+
+func FilePutContents(fn string, con []byte) ([]byte, int, error) {
+  var size int
+  if strings.HasPrefix(fn, "http://") {
+    body, err := HPost(fn, string(con), "")
+    return body, len(body), err
+  }
   fp, err := os.OpenFile(fn, os.O_RDWR|os.O_CREATE, os.ModePerm)
   if err != nil {
-    return err
+    return []byte(""), 0, err
   }
   defer fp.Close()
-  _, err = fp.Write(con)
-  return err
+  size, err = fp.Write(con)
+  return []byte(""), size, err
 }
 
 func FileAppendContents(fn string, con []byte) error {
